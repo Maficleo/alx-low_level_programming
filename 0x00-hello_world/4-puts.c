aea53a57709a9b814a10d9f01d29a3d1dd2c71c7
Maficleo <maficleo11@gmail.com>
2022-03-14 14:29:08 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- *main - entry point
+
+ * main - Entry point
+
  *
- *Return: 0 after printing the function
+
+ * Return: Always 0 (Success)
+
  */
-int main (void)
+
+int main(void)
+
 {
-puts('/'Programming is like buidling a multilingual puzzle');
-return '0';
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
